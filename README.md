aeb93acde5bf3611198a37ab6efbbdfd71cb7df4
wobsoriano <sorianorobertc@gmail.com>
2025-01-21 10:31:00 -0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Express SDK examples
+
+1. Add keys to `.env`
+2. Update `public/index.html` and add your publishable key again, plus your YOUR_FRONTEND_API_URL
+3. Run `npm run dev`
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import 'dotenv/config'
+import express from 'express'
+import { clerkClient, clerkMiddleware, getAuth, requireAuth } from '@clerk/express'
+import { join } from 'path';
+
+const app = express()
+const PORT = 3002
+
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => {
+  res.sendFile(join(process.cwd(), 'public/index.html'));
+})
+
+// Test https://clerk.com/docs/pr/1880/references/backend/sessions/get-token#examples-with-frameworks
+// Make sure to create a JWT template with `test` as name
+// https://clerk.com/docs/backend-requests/making/jwt-templates
+app.get('/get-token', requireAuth(), async (req, res) => {
+  const { sessionId } = req.auth
+
+  if (!sessionId) {
+    res.status(401).json({ error: 'Unauthorized' })
+    return
+  }
+
+  const template = 'test'
+
+  const token = await clerkClient.sessions.getToken(sessionId, template)
+
+  res.json({ token })
+})
+
+// Test https://clerk.com/docs/pr/1880/users/user-impersonation#detect-impersonated-sessions-in-the-backend
+// Impersonate a user. Follow the instructions here https://clerk.com/docs/users/user-impersonation
+app.get('/get-impersonated-user', requireAuth(), async (req, res) => {
+  const { userId, actor } = req.auth
+
+  if (!userId) {
+    res.status(401).json({ error: 'Unauthorized' })
+    return
+  }
+
+  res.json({ userId, actor })
+})
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
